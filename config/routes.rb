d434b4b3732b7164c9eade0e615b9892d4f87f78
Kohei-Oki <kohei.goldcoast0702@gmail.com>
2018-03-08 10:25:51 +0800

    Sign Up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
 	root 'static_pages#home'
+	get '/signup', to:'users#new', as: 'signup'
+  	get '/login', to:'sessions#new', as: 'login'
+  	post '/login', to:'sessions#create'
+  	delete '/logout', to:'sessions#destroy'
+
+  	resources :users
+  	resources :password_resets, only: [:new, :create, :edit, :update]
 end
