816b0230cb696df6680a694a39bfcd8a4784ef2f
Kohei-Oki <kohei.goldcoast0702@gmail.com>
2018-03-08 19:33:48 +0800

    Log in fucntion

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,20 @@ class SessionsController < ApplicationController
 
 	def create
 		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			log_in user
+		#params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+			redirect_to root_url
+			flash[:success] = "You've logged in!"
+		else
+			flash.now[:danger] = 'Invalid email/password combination'
+			render 'new'
+		end
 	end
 
 	def destroy
 		log_out
+		flash[:info] = "Why did you log out?"
 		redirect_to root_url
 	end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,40 @@
 module SessionsHelper
+	def log_in(user)
+		session[:user_id] = user.id
+	end
+
+	def current_user
+		if(user_id = session[:user_id])
+			@current_user ||= User.find_by(id: session[:user_id])
+		elsif (user_id = cookies.signed[:user_id])
+			user = User.find_by(id: user_id)
+			if user && user.authenticated?(:remember, cookies[:remember_token])
+				log_in user
+				@current_user
+			end
+		end
+	end
+
+	def current_user?(user)
+		current_user == user
+	end
+
+	def redirect_back_or(default)
+		redirect_to(session[:forwarding_url] || default)
+		session.delete(:forwarding_url)
+	end
+
+	def store_location
+		session[:forwarding_url] = request.url if request.get?
+	end
+
+	def logged_in?
+		!current_user.nil?
+	end
+
+	def log_out
+		session.delete(:user_id)
+		@current_user = nil
+	end
+
 end
