28e5f5a589b92cd23a1203848a6f3fb85e1cf736
Kohei-Oki <kohei.goldcoast0702@gmail.com>
2018-03-26 12:38:59 +0800

    [RoR]Fixed User Detail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,88 +1,88 @@
 class UsersController < ApplicationController
-	 before_action	:logged_in_user,	only: [:index, :edit, :update]
-	 before_action	:correct_user,		only: [:edit, :update]
-	 before_action	:admin_user,			only: :destroy
+   before_action  :logged_in_user,  only: [:index, :edit, :update]
+   before_action  :correct_user,    only: [:edit, :update]
+   before_action  :admin_user,      only: :destroy
 
-	def index
+  def index
     @users = User.paginate(page: params[:page])
-	end
+  end
 
-	def show
-		@user = User.find(params[:id])
-	end
+  def show
+    @user = User.find(params[:id])
+  end
 
-	def new
-		@user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
-	def create
-		@user = User.new(user_params)
-		if @user.save
-			redirect_to root_url
-			flash[:info] = "Your account is created!"
-		else
-			render 'new'
-		end
-	end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_url
+      flash[:info] = "Your account is created!"
+    else
+      render 'new'
+    end
+  end
 
-	def edit
-		@user = User.find(params[:id])
-	end
+  def edit
+    @user = User.find(params[:id])
+  end
 
-	def update
-		@user = User.find(params[:id])
-		if @user.update_attributes(user_params)
-			flash[:success] = "Profile updated"
-			redirect_to @user
-		else
-			render 'edit'
-		end
-	end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
-	def destroy
-		User.find(params[:id]).destroy
-		flash[:success] = "User deleted"
-		redirect_to users_url
-	end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
 
-	def following
-		@title = "Following"
-		@user = User.find(params[:id])
-		@users = @user.following.paginate(page: params[:page])
-		render 'show_follow'
-	end
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
 
-	def followers
-		@title = "Followers"
-		@user = User.find(params[:id])
-		@users = @user.followers.paginate(page: params[:page])
-		render 'show_follow'
-	end
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
 
-	def logged_in_user
-		unless logged_in?
-			store_location
-			flash[:danger] = "Please log in"
-			redirect_to login_url
-		end
-	end
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in"
+      redirect_to login_url
+    end
+  end
 
-	def correct_user
-		@user = User.find(params[:id])
-		redirect_to(root_url) unless @user == current_user
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
 
-			if(!@user == current_user)
-				redirect_to(root_url)
-			end
-	end
+      if(!@user == current_user)
+        redirect_to(root_url)
+      end
+  end
 
-	def admin_user
-		redirect_to(root_url) unless current_user.admin?
-	end
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 
  private
- 	def user_params
- 		params.require(:user).permit(:name, :email, :password, :password_confirmation)
- 	end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 
 end
