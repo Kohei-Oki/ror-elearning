24528edc86e5e67becce0057231e536616747ca9
Kohei-Oki <kohei.goldcoast0702@gmail.com>
2018-03-27 11:50:14 +0800

    [RoR]Created Activity record in Home & User show

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
-	def home
-		if logged_in?
-		end
-	end
+  def home
+    if logged_in?
+      @lessons = current_user.lessons
+      @feed_items = current_user.feed
+    end
+  end
+
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,6 +4,8 @@ class Lesson < ApplicationRecord
   has_many :lesson_words, dependent: :destroy
   has_many :word_answers, through: :lesson_words
 
+  default_scope -> { order(created_at: :desc)}
+
   def correct_answers
     self.word_answers.where(is_correct: true)
   end
