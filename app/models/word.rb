d379010ea5516861e1ab7ce6b5f2990c940396fc
Kohei-Oki <kohei.goldcoast0702@gmail.com>
2018-03-28 10:29:31 +0800

    [RoR]Fixed Word List

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,4 +2,9 @@ class Word < ApplicationRecord
   belongs_to :category
   has_many :word_answers, dependent: :destroy
   has_many :lesson_words
+
+  def correct_answer
+    self.word_answers.find_by(is_correct: true)
+  end
+
 end
